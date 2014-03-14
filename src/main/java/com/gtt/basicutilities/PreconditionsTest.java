6826fc68bf3d4dcdde7ca0d1b59ed93f1fce380d
tiantian.gao <tiantian.gao@dianping.com>
2014-03-14 18:52:49 +0800

    参数检查

--- src/main/java/com/gtt/basicutilities/PreconditionsTest.java
+++ src/main/java/com/gtt/basicutilities/PreconditionsTest.java
@@ -1,15 +1,49 @@
 package com.gtt.basicutilities;
 
-import com.google.common.base.Preconditions;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 /**
- *
+ * 
  * @author tiantiangao
  */
 public class PreconditionsTest {
 
 	@Test
-	public void test(){
+	public void test() {
+		testCheckArgument();
+		testCheckNotNull();
+
+	}
+
+	private void testCheckArgument() {
+		int i = 1;
+		checkArgument(i > 0, "参数是%s, 参数必须为正整数", i);
+
+		try {
+			i = -1;
+			checkArgument(-1 > 0, "参数是%s, 参数必须为正整数", -1);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+	}
+
+	private void testCheckNotNull() {
+		Object value = new Object();
+
+		checkNotNull(value, "参数是null");
+
+		try {
+			value = null;
+			checkNotNull(value, "参数是null");
+			fail();
+		} catch (NullPointerException e) {
+			assertTrue(true);
+		}
 	}
 }
