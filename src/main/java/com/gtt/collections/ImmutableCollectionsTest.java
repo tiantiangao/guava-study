8567bb2acbd1419da6a77d9c78fc27691eb32bfe
tiantian.gao <tiantian.gao@dianping.com>
2014-03-18 18:55:54 +0800

    不可变集合

--- src/main/java/com/gtt/collections/ImmutableCollectionsTest.java
+++ src/main/java/com/gtt/collections/ImmutableCollectionsTest.java
@@ -2,14 +2,13 @@ package com.gtt.collections;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
+import com.google.common.collect.*;
 
 /**
  * @author tiantiangao
@@ -19,6 +18,7 @@ public class ImmutableCollectionsTest {
 	@Test
 	public void test() {
 		testJDKUnmodifiedList();
+		testCreate();
 		testAsList();
 	}
 
@@ -32,10 +32,36 @@ public class ImmutableCollectionsTest {
 		assertEquals(4, unmodifiedLists.size());
 	}
 
+	private void testCreate() {
+		testCopyOf();
+		testOf();
+		testBuilder();
+	}
+
+	private void testCopyOf() {
+		ArrayList<Integer> list = Lists.newArrayList(1, 2, 3);
+		ImmutableList<Integer> unmodifiedList = ImmutableList.copyOf(list);
+		assertEquals(3, unmodifiedList.size());
+
+		list.add(4);
+		assertEquals(3, unmodifiedList.size());
+	}
+
+	private void testOf() {
+		assertEquals(4, ImmutableList.of(1, 2, 3, 4).size());
+		assertEquals(4, ImmutableSet.of(1, 2, 3, 4).size());
+		assertEquals(4, ImmutableMap.of("aa", 1, "bb", 2, "cc", 3, "dd", 4).entrySet().size());
+		assertEquals(4, (Object) ImmutableMap.of("aa", 1, "bb", 2, "cc", 3, "dd", 4).get("dd"));
+	}
+
+	private void testBuilder() {
+	}
+
 	private void testAsList() {
-		ImmutableSet<Integer> iset= ImmutableSet.of(1, 2, 3, 4, 5);
-		ImmutableList<Integer> ilist= iset.asList();
-		System.out.println(iset);
-		System.out.println(ilist.get(2));
+		ImmutableSortedSet<Integer> iset = ImmutableSortedSet.of(5, 2, 3, 4, 1);
+		ImmutableList<Integer> ilist = iset.asList();
+
+		List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
+		assertEquals(list, ilist);
 	}
 }
